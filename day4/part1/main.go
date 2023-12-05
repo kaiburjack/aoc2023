8a9352b910becb10d486fe9103a4f257d3662821
Kai Burjack <kai.burjack@hbt.de>
2023-12-05 19:16:45 +0100

    Refactor day4 solutions using parser generator

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -2,92 +2,38 @@ package main
 
 import (
 	"bufio"
-	"io"
+	"github.com/alecthomas/participle/v2"
 	"os"
+	"slices"
 )
 
-type parser struct {
-	r *bufio.Reader
+type Input struct {
+	Cards []Card `@@+`
 }
 
-func (p *parser) expect(s string) error {
-	for i := 0; i < len(s); i++ {
-		_, err := p.r.ReadByte()
-		if err == io.EOF {
-			return err
-		}
-	}
-	return nil
+type Card struct {
+	WinningNumbers []int `"Card" Int+ ":" @Int+`
+	MyNumbers      []int `"|" @Int+`
 }
 
-func (p *parser) integer() int {
-	var n int
-	for {
-		b, _ := p.r.ReadByte()
-		if b >= '0' && b <= '9' {
-			n = n*10 + int(b-'0')
-		} else {
-			_ = p.r.UnreadByte()
-			return n
-		}
-	}
-}
-
-func (p *parser) ws() {
-	for {
-		b, _ := p.r.ReadByte()
-		if b != ' ' {
-			_ = p.r.UnreadByte()
-			break
-		}
-	}
-}
-
-func (p *parser) card() (int, error) {
-	winningNumbers := make(map[int]bool)
+func points(c Card) int {
 	points := 0
-	err := p.expect("Card")
-	if err == io.EOF {
-		return 0, err
-	}
-	p.ws()
-	_ = p.integer()
-	_ = p.expect(":")
-	var state int
-	for {
-		p.ws()
-		b, _ := p.r.ReadByte()
-		if b == '|' {
-			state = 1
-		} else if b == '\n' {
-			break
-		} else {
-			_ = p.r.UnreadByte()
-		}
-		p.ws()
-		n := p.integer()
-		if state == 0 {
-			winningNumbers[n] = true
-		} else {
-			if winningNumbers[n] {
-				points = max(points<<1, 1)
-			}
+	for _, n := range c.WinningNumbers {
+		if slices.Contains(c.MyNumbers, n) {
+			points = max(points<<1, 1)
 		}
 	}
-	return points, nil
+	return points
 }
 
 func main() {
-	file, _ := os.OpenFile("input.txt", os.O_RDONLY, 0)
-	r := bufio.NewReader(file)
-	p := parser{r}
+	fileName := "input.txt"
+	file, _ := os.OpenFile(fileName, os.O_RDONLY, 0)
+	parser, _ := participle.Build[Input]()
+	input, _ := parser.Parse(fileName, bufio.NewReader(file))
 	sumOfPoints := 0
-	for {
-		points, err := p.card()
-		if err == io.EOF {
-			break
-		}
-		sumOfPoints += points
+	for _, c := range input.Cards {
+		sumOfPoints += points(c)
 	}
 	println(sumOfPoints)
 }
--- day4/part2/main.go
+++ day4/part2/main.go
@@ -2,92 +2,39 @@ package main
 
 import (
 	"bufio"
-	"io"
+	"github.com/alecthomas/participle/v2"
 	"os"
+	"slices"
 )
 
-type parser struct {
-	r *bufio.Reader
+type Input struct {
+	Cards []Card `@@+`
 }
 
-func (p *parser) expect(s string) error {
-	for i := 0; i < len(s); i++ {
-		_, err := p.r.ReadByte()
-		if err == io.EOF {
-			return err
-		}
-	}
-	return nil
+type Card struct {
+	WinningNumbers []int `"Card" Int+ ":" @Int+`
+	MyNumbers      []int `"|" @Int+`
 }
 
-func (p *parser) integer() int {
-	var n int
-	for {
-		b, _ := p.r.ReadByte()
-		if b >= '0' && b <= '9' {
-			n = n*10 + int(b-'0')
-		} else {
-			_ = p.r.UnreadByte()
-			return n
+func numberOfWinningNumbers(c Card) int {
+	num := 0
+	for _, n := range c.WinningNumbers {
+		if slices.Contains(c.MyNumbers, n) {
+			num++
 		}
 	}
-}
-
-func (p *parser) ws() {
-	for {
-		b, _ := p.r.ReadByte()
-		if b != ' ' {
-			_ = p.r.UnreadByte()
-			break
-		}
-	}
-}
-
-func (p *parser) card() (int, error) {
-	winningNumbers := make(map[int]bool)
-	numberOfWinningNumbers := 0
-	err := p.expect("Card")
-	if err == io.EOF {
-		return 0, err
-	}
-	p.ws()
-	_ = p.integer()
-	_ = p.expect(":")
-	var state int
-	for {
-		p.ws()
-		b, _ := p.r.ReadByte()
-		if b == '|' {
-			state = 1
-		} else if b == '\n' {
-			break
-		} else {
-			_ = p.r.UnreadByte()
-		}
-		p.ws()
-		n := p.integer()
-		if state == 0 {
-			winningNumbers[n] = true
-		} else {
-			if winningNumbers[n] {
-				numberOfWinningNumbers++
-			}
-		}
-	}
-	return numberOfWinningNumbers, nil
+	return num
 }
 
 func main() {
-	file, _ := os.OpenFile("input.txt", os.O_RDONLY, 0)
-	r := bufio.NewReader(file)
-	p := parser{r}
+	fileName := "input.txt"
+	file, _ := os.OpenFile(fileName, os.O_RDONLY, 0)
+	parser, _ := participle.Build[Input]()
+	input, _ := parser.Parse(fileName, bufio.NewReader(file))
 	var wonCopies []int64
 	var numWonCards int64 = 0
-	for {
-		numberOfWinningNumbers, err := p.card()
-		if err == io.EOF {
-			break
-		}
+	for _, c := range input.Cards {
+		numberOfWinningNumbers := numberOfWinningNumbers(c)
 		var count int64 = 1
 		if len(wonCopies) > 0 {
 			count = 1 + wonCopies[0]
