f0a4641840fe07fd5f3666e792b23cae2b541af9
Kai Burjack <kai.burjack@hbt.de>
2023-12-09 06:45:49 +0100

    Add day9

---
+++ day9/part1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func extrapolationsSum(numbers []int64) int64 {
+	if len(numbers) <= 1 {
+		return 0
+	}
+	differences := make([]int64, len(numbers)-1)
+	for i := 0; i < len(numbers)-1; i++ {
+		differences[i] = numbers[i+1] - numbers[i]
+	}
+	return numbers[len(numbers)-1] + extrapolationsSum(differences)
+}
+
+func main() {
+	readFile, _ := os.Open("input.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var sumOfSums int64
+	for fileScanner.Scan() {
+		words := strings.Split(fileScanner.Text(), " ")
+		numbers := make([]int64, len(words))
+		for i, word := range words {
+			numbers[i], _ = strconv.ParseInt(word, 10, 64)
+		}
+		sumOfSums += extrapolationsSum(numbers)
+	}
+	println(sumOfSums)
+}
