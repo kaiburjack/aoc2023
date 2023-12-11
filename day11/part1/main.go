f32bd7c1596633bfcfed98b0ae96de24f1f72369
Kai Burjack <kai.burjack@hbt.de>
2023-12-11 08:03:50 +0100

    Add day11

---
+++ day11/part1/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"math"
+	"os"
+)
+
+type coord struct {
+	x, y int
+	ox   int
+}
+
+func manhattanDistance(c1, c2 coord) int {
+	return int(math.Abs(float64(c1.ox-c2.ox))) + int(math.Abs(float64(c1.y-c2.y)))
+}
+
+func main() {
+	readFile, _ := os.Open("input.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	galaxies := make([]*coord, 0)
+
+	// parse map and do y-offsetting already
+	y := 0
+	colHadGalaxy := make([]bool, 1024) // <- just have a large enough array
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		emptyRow := true
+		for x, c := range line {
+			if c == '#' {
+				// found a galaxy
+				galaxies = append(galaxies, &coord{x, y, x})
+				// column cannot be empty anymore
+				colHadGalaxy[x] = true
+				// row cannot be empty anymore
+				emptyRow = false
+			}
+		}
+		if emptyRow {
+			// offset y for later rows
+			y++
+		}
+		y++
+	}
+
+	// fix empty columns and offset items
+	// with x coordinate greater than that of
+	// the empty column
+	for x, hadGalaxy := range colHadGalaxy {
+		if !hadGalaxy {
+			for _, c := range galaxies {
+				if c.x > x {
+					c.ox++
+				}
+			}
+		}
+	}
+
+	// compute all manhattan distances
+	// between pairs of galaxies
+	sumOfShortesPaths := 0
+	for i, gi := range galaxies {
+		for _, gj := range galaxies[:i] {
+			sumOfShortesPaths += manhattanDistance(*gi, *gj)
+		}
+	}
+
+	println(sumOfShortesPaths)
+}
---
+++ day11/part2/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"math"
+	"os"
+)
+
+type coord struct {
+	x, y int64
+	ox   int64
+}
+
+func manhattanDistance(c1, c2 coord) int64 {
+	return int64(math.Abs(float64(c1.ox-c2.ox))) + int64(math.Abs(float64(c1.y-c2.y)))
+}
+
+func main() {
+	readFile, _ := os.Open("input.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	galaxies := make([]*coord, 0)
+
+	// parse map and do y-offsetting already
+	var y int64
+	var scale int64 = 1000000
+	colHadGalaxy := make([]bool, 1024) // <- just have a large enough array
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		emptyLine := true
+		for x, c := range line {
+			if c == '#' {
+				// found a galaxy
+				galaxies = append(galaxies, &coord{int64(x), y, int64(x)})
+				// column cannot be empty anymore
+				colHadGalaxy[x] = true
+				// row cannot be empty anymore
+				emptyLine = false
+			}
+		}
+		if emptyLine {
+			// offset y for later rows
+			// the - 1 here is all of the trick of this part!
+			// because in the first part we _actually_ made every
+			// empty row TWICE as large (by adding one empty line)
+			// so the scale was 2 in the first part, and therefore
+			// the _increment_ was 1
+			y += scale - 1
+		}
+		y++
+	}
+
+	// find empty columns and offset items
+	// with x coordinate greater than that of
+	// the empty column
+	for x, hadGalaxy := range colHadGalaxy {
+		if !hadGalaxy {
+			for _, c := range galaxies {
+				if c.x > int64(x) {
+					// the - 1 here is all of the trick of this part!
+					// because in the first part we _actually_ made every
+					// empty column TWICE as large (by adding one empty column)
+					// so the scale was 2 in the first part, and therefore
+					// the _increment_ was 1
+					c.ox += scale - 1
+				}
+			}
+		}
+	}
+
+	// compute all manhattan distances
+	// between pairs of galaxies
+	var sumOfShortesPaths int64
+	for i, gi := range galaxies {
+		for _, gj := range galaxies[:i] {
+			sumOfShortesPaths += manhattanDistance(*gi, *gj)
+		}
+	}
+
+	println(sumOfShortesPaths)
+}
