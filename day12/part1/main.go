53b8bcee969e05b3a812b2d74ea7b9552e043163
Kai Burjack <kai.burjack@hbt.de>
2023-12-12 18:00:25 +0100

    Add comments to day12 part1

--- day12/part1/main.go
+++ day12/part1/main.go
@@ -7,43 +7,81 @@ import (
 	"strings"
 )
 
+// matchesCount returns the solution for the given input string
+// and number of damaged springs by recursing over the input string
+// and accumulating the number of valid combinations that we can
+// generate.
 func matchesCount(s string, numDefects []int) int64 {
 	if len(s) == 0 && len(numDefects) == 0 {
+		// if we have no more characters to process and no more
+		// damaged springs to find, we return "1 combination"
+		// for the processed input/path until here.
 		return 1
 	} else if len(s) == 0 {
+		// if we have no more characters to process, but still
+		// have damaged springs to find, we return "zero combinations"
 		return 0
 	}
+
 	switch s[0] {
 	case '.':
+		// this is a normal non-damanged spring, which we can ignore
+		// we continue to recurse over the remaining input.
 		return matchesCount(s[1:], numDefects)
 	case '?':
+		// this is a spring that may or may not be damaged.
 		if len(numDefects) == 0 {
+			// if we have no more damaged springs to find, we
+			// interpret this as a normal non-damanged spring (.)
+			// and continue to recurse over the remaining input.
 			return matchesCount(s[1:], numDefects)
 		}
+		// otherwise, we need to consider two possible paths:
+		// 1. this is a damaged spring, and we need to complete the sequence
+		//    of 'num' damaged springs before we can continue to recurse.
+		// 2. this is a normal non-damaged spring, and we can continue to
+		//    recurse over the remaining input (like in the case '.' above).
 		return mustBeDefect(s, numDefects, numDefects[0]) + matchesCount(s[1:], numDefects)
 	case '#':
+		// this is a damaged spring, and we need to complete the sequence
+		// of necessary damaged springs before we can continue.
 		if len(numDefects) == 0 {
+			// if we have no more damaged springs to find, we cannot generate
+			// a possible combination anymore, and return "zero combinations"
 			return 0
 		}
+		// otherwise, we need to complete the sequence of 'num' damaged springs.
+		// because this is a common case here and above, it is in its own function.
 		return mustBeDefect(s, numDefects, numDefects[0])
 	}
 	return 0
 }
 
+// mustBeDefect is called when we know that we must generate _exactly_
+// num damaged springs to now recurse per character.
 func mustBeDefect(s string, numDefects []int, num int) int64 {
 	if len(s) < num {
+		// when we have less characters than we need to generate
+		// defects, return "zero combinations"
 		return 0
 	}
 	for i := 0; i < num; i++ {
 		if s[i] != '#' && s[i] != '?' {
+			// if any of the needed characters cannot be made
+			// into a damaged spring, return "zero combinations"
 			return 0
 		}
 	}
 	if len(s) == num {
+		// if we have exactly the number of characters we need
+		// may be done now, but let that be detected by matchesCount().
 		return matchesCount(s[num:], numDefects[1:])
 	} else if s[num] == '.' || s[num] == '?' {
+		// if there are more characters, the next one after the damaged
+		// springs must be a normal spring.
 		return matchesCount(s[num+1:], numDefects[1:])
 	}
+	// any other case, and we return "zero combinations"
 	return 0
 }
 
@@ -53,12 +91,15 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 	var matchesCountSum int64
 	for fileScanner.Scan() {
+		// parse each line as an input string and a list of
+		// numbers of damaged springs
 		splitBySpace := strings.Split(fileScanner.Text(), " ")
 		var damagedCount []int
 		for _, number := range strings.Split(splitBySpace[1], ",") {
 			n, _ := strconv.Atoi(strings.TrimSpace(number))
 			damagedCount = append(damagedCount, n)
 		}
+		// accumulate the number of combinations for each line
 		matchesCountSum += matchesCount(splitBySpace[0], damagedCount)
 	}
 	println(matchesCountSum)
