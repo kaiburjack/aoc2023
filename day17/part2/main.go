318fab05903e893c6512658430dfe7598c470525
Kai Burjack <kai.burjack@hbt.de>
2023-12-17 10:29:27 +0100

    refactor: use pointer types to reduce number of copied bytes
    when reading/writing from/to the priority queue.

--- day17/part2/main.go
+++ day17/part2/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // build a priority queue using the container/heap package
-type priorityQueue []pathVertex
+type priorityQueue []*pathVertex
 
 func (v *priorityQueue) Less(i, j int) bool {
 	return (*v)[i].d < (*v)[j].d
@@ -20,7 +20,7 @@ func (v *priorityQueue) Swap(i, j int) {
 	(*v)[i], (*v)[j] = (*v)[j], (*v)[i]
 }
 func (v *priorityQueue) Push(x interface{}) {
-	*v = append(*v, x.(pathVertex))
+	*v = append(*v, x.(*pathVertex))
 }
 func (v *priorityQueue) Pop() interface{} {
 	old := *v
@@ -32,7 +32,7 @@ func (v *priorityQueue) Pop() interface{} {
 
 // vertices that we follow via the priority queue and which form the path
 type pathVertex struct {
-	d      int
+	d      uint
 	x, y   uint8
 	dx, dy int8
 }
@@ -60,13 +60,13 @@ func main() {
 	w, h := uint8(len(grid[0])), uint8(len(grid))
 
 	var q priorityQueue
-	heap.Push(&q, pathVertex{})
+	heap.Push(&q, &pathVertex{})
 	var visited = make(map[visitedVertex]struct{})
-	var c pathVertex
+	var c *pathVertex
 	const MIN, MAX = 4, 10
 	// we need to visit some more nodes
 	for len(q) > 0 {
-		c = heap.Pop(&q).(pathVertex)
+		c = heap.Pop(&q).(*pathVertex)
 		// check if c vertex is the end and we reached it with the minimum amount of steps in the same direction
 		if c.x == w-1 && c.y == h-1 && (math.Abs(float64(c.dx)) >= MIN || math.Abs(float64(c.dy)) >= MIN) {
 			break
@@ -76,33 +76,33 @@ func main() {
 		if c.x > 0 && (c.dx == 0 && c.dy == 0 || c.dx < 0 || math.Abs(float64(c.dy)) >= MIN) && c.dx > -MAX {
 			v := visitedVertex{c.x - 1, c.y, c.dx - 1, 0}
 			if _, ok := visited[v]; !ok {
-				nextCost := c.d + int(grid[c.y][c.x-1])
+				nextCost := c.d + uint(grid[c.y][c.x-1])
 				visited[v] = struct{}{}
-				heap.Push(&q, pathVertex{nextCost, c.x - 1, c.y, c.dx - 1, 0})
+				heap.Push(&q, &pathVertex{nextCost, c.x - 1, c.y, c.dx - 1, 0})
 			}
 		}
 		if c.x < w-1 && (c.dx == 0 && c.dy == 0 || c.dx > 0 || math.Abs(float64(c.dy)) >= MIN) && c.dx < MAX {
 			v := visitedVertex{c.x + 1, c.y, c.dx + 1, 0}
 			if _, ok := visited[v]; !ok {
-				nextCost := c.d + int(grid[c.y][c.x+1])
+				nextCost := c.d + uint(grid[c.y][c.x+1])
 				visited[v] = struct{}{}
-				heap.Push(&q, pathVertex{nextCost, c.x + 1, c.y, c.dx + 1, 0})
+				heap.Push(&q, &pathVertex{nextCost, c.x + 1, c.y, c.dx + 1, 0})
 			}
 		}
 		if c.y > 0 && (c.dx == 0 && c.dy == 0 || c.dy < 0 || math.Abs(float64(c.dx)) >= MIN) && c.dy > -MAX {
 			v := visitedVertex{c.x, c.y - 1, 0, c.dy - 1}
 			if _, ok := visited[v]; !ok {
-				nextCost := c.d + int(grid[c.y-1][c.x])
+				nextCost := c.d + uint(grid[c.y-1][c.x])
 				visited[v] = struct{}{}
-				heap.Push(&q, pathVertex{nextCost, c.x, c.y - 1, 0, c.dy - 1})
+				heap.Push(&q, &pathVertex{nextCost, c.x, c.y - 1, 0, c.dy - 1})
 			}
 		}
 		if c.y < h-1 && (c.dx == 0 && c.dy == 0 || c.dy > 0 || math.Abs(float64(c.dx)) >= MIN) && c.dy < MAX {
 			v := visitedVertex{c.x, c.y + 1, 0, c.dy + 1}
 			if _, ok := visited[v]; !ok {
-				nextCost := c.d + int(grid[c.y+1][c.x])
+				nextCost := c.d + uint(grid[c.y+1][c.x])
 				visited[v] = struct{}{}
-				heap.Push(&q, pathVertex{nextCost, c.x, c.y + 1, 0, c.dy + 1})
+				heap.Push(&q, &pathVertex{nextCost, c.x, c.y + 1, 0, c.dy + 1})
 			}
 		}
 	}
