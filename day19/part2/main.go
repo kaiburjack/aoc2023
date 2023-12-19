32e5b056896a37a07fc37e270c0c85ffbda0410c
Kai Burjack <kai.burjack@hbt.de>
2023-12-19 15:21:25 +0100

    refactor: extract common code into function and inline variables

--- day19/part2/main.go
+++ day19/part2/main.go
@@ -22,37 +22,29 @@ type Input struct {
 
 var cat2idx = map[string]int{"x": 0, "m": 1, "a": 2, "s": 3}
 
-func combinations(min, max []int) int64 {
-	return int64((max[0]-min[0])+1) * int64((max[1]-min[1])+1) * int64((max[2]-min[2])+1) * int64((max[3]-min[3])+1)
+func sumUp(min []int, max []int, cat string, n2w map[string]Workflow) int64 {
+	if cat == "A" {
+		return int64((max[0]-min[0])+1) * int64((max[1]-min[1])+1) * int64((max[2]-min[2])+1) * int64((max[3]-min[3])+1)
+	} else if cat != "R" {
+		return validCombinations(min, max, n2w[cat], n2w)
+	}
+	return 0
 }
 func validCombinations(min, max []int, w Workflow, n2w map[string]Workflow) int64 {
 	var sum int64
 	for _, r := range w.Rules {
 		if r.Op == "" {
-			if r.Cat == "A" {
-				sum += combinations(min, max)
-			} else if r.Cat != "R" {
-				sum += validCombinations(min, max, n2w[r.Cat], n2w)
-			}
-			return sum
+			return sum + sumUp(min, max, r.Cat, n2w)
 		}
 		ci := cat2idx[r.Cat]
-		newMin, newMax := min[ci], max[ci]
-		nextMin, nextMax := min[ci], max[ci]
-		if r.Op == "<" {
-			newMax, nextMin = r.Num-1, r.Num
-		} else if r.Op == ">" {
-			newMin, nextMax = r.Num+1, r.Num
-		}
 		mins := []int{min[0], min[1], min[2], min[3]}
 		maxs := []int{max[0], max[1], max[2], max[3]}
-		mins[ci], maxs[ci] = newMin, newMax
-		if r.Dest == "A" {
-			sum += combinations(mins, maxs)
-		} else if r.Dest != "R" {
-			sum += validCombinations(mins, maxs, n2w[r.Dest], n2w)
+		if r.Op == "<" {
+			maxs[ci], min[ci] = r.Num-1, r.Num
+		} else if r.Op == ">" {
+			mins[ci], max[ci] = r.Num+1, r.Num
 		}
-		min[ci], max[ci] = nextMin, nextMax
+		sum += sumUp(mins, maxs, r.Dest, n2w)
 	}
 	return sum
 }
